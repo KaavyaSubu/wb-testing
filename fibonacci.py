49042dedc1f609928b867393256bb9aa182ad595
KaavyaSubu <subramka@oregonstate.edu>
2021-03-11 09:38:20 -0800

    Working with pytest and unittest

--- fibonacci.py
+++ fibonacci.py
@@ -3,8 +3,10 @@ def printFib():
 
     fibList = [0,1]
 
-    while len(fibList <= 10):
+    while len(fibList) < 10:
         fibList.append(fibList[len(fibList) - 1] + fibList[len(fibList) - 2])
 
     return fibList
 
+
+print(printFib())
---
+++ unittest_fibonacci.py
@@ -0,0 +1,18 @@
+import unittest
+import fibonacci
+
+
+
+class TestCase(unittest.TestCase):
+
+    def fib(self):
+        expectedList = [0,1,1,2,3,5,8,13,21,34]
+        self.assertEqual(fibonacci.printFib(),expectedList)
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
