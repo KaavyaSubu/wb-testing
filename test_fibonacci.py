9a49a24e0be49a9328139b907fe75eb7dd424886
KaavyaSubu <subramka@oregonstate.edu>
2021-03-11 09:22:25 -0800

    Wrote a test for fibonacci

---
+++ test_fibonacci.py
@@ -0,0 +1,18 @@
+import fibonacci
+import unittest
+
+
+
+class TestCase(unittest.TestCase):
+
+    def fib(self):
+        expectedList = [0,1,1,2,3,5,8,13,21,34]
+        self.assertEqual(fibonacci.print(),expectedList)
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
